a61869f8538c75b719f3053eb244e132667adb45
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-09-16 22:03:31 +0800

    event categories section done

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -15,7 +15,38 @@ function EventPlanner() {
                 <button className='get-started-button'>Get Started</button>
             </section>
 
-            <section className='event_categories'></section>
+            <section className='events_categories'>
+                <ul>
+                    <h2>Social Events:</h2>
+                    <li>Birthday</li>
+                    <li>Anniversary</li>
+                    <li>Wedding</li>
+                    <li>Baby</li>
+                    <li>Graduation</li>
+                    <li>Family</li>
+                </ul>
+
+                <ul>
+                    <h2>Entertainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music</li>
+                    <li>Film</li>
+                    <li>Comedy</li>
+                    <li>Art</li>
+                    <li>Cultural</li>
+                </ul>
+
+                <ul>
+                    <h2>Community Events:</h2>
+                    <li>Fundraising</li>
+                    <li>Charity</li>
+                    <li>Volunteer</li>
+                    <li>Neighorhood</li>
+                    <li>Community</li>
+                    <li>Cultural</li>
+                </ul>
+            </section>
+
             <section className='features'></section>
             <section className='testimonials'></section>
             <section className='contact'></section>
