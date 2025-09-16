f6312fe6151e5c8b8c3372cd35c11c54d0213bbb
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-09-16 21:53:55 +0800

    content scafold

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './EventPlanner.css'; // Import CSS file for styling
 
-const EventPlanner = () => {
+function EventPlanner() {
     return (
         <div className="event-planner-container">
+            {/* header */}
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+
+            <section className='description'></section>
+            <section className='event_categories'></section>
+            <section className='features'></section>
+            <section className='testimonials'></section>
+            <section className='contact'></section>
         </div>
     );
 };
