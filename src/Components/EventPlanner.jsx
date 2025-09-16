682db953611a0693f7020b8a75ba51286ebc7a8d
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-09-16 21:58:22 +0800

    description section done

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -9,7 +9,12 @@ function EventPlanner() {
                 <h1>Welcome to Event Planner</h1>
             </header>
 
-            <section className='description'></section>
+            <section className='description'>
+                <p>Plan and organize your events offortlessly with Event Planner. From birthdays to corporate meetings, we've got you covered.
+                </p>
+                <button className='get-started-button'>Get Started</button>
+            </section>
+
             <section className='event_categories'></section>
             <section className='features'></section>
             <section className='testimonials'></section>
